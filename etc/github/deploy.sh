cabbeb54ced2668aabb2960359ec822833618542
Jiho Lee <>
2020-05-29 14:05:41 -0700

    Update deploy to deploy

--- etc/github/deploy.sh
+++ etc/github/deploy.sh
@@ -1,5 +1,3 @@
-# eval $(ssh-agent)
-# ssh-add -k /usr/share/httpd/.ssh/id_rsa_ntwglobal_ople-backoffice
-# /usr/bin/git reset --hard origin/master
+/usr/bin/git reset --hard origin/master
 # /usr/bin/git clean -f -d
 /usr/bin/git pull origin master
