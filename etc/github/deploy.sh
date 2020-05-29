c0faaa0116f440009e3e39918781c6bd2c6565e5
Jiho Lee <>
2020-05-29 14:25:34 -0700

    Update deploy to deploy

--- etc/github/deploy.sh
+++ etc/github/deploy.sh
@@ -1,3 +1,5 @@
-/usr/bin/git reset --hard origin/master
+/usr/bin/git config --global user.name "Jiho Lee"
+/usr/bin/git config --global user.email "jiho.lee@ntwglobal.com"
+# /usr/bin/git reset --hard origin/master
 # /usr/bin/git clean -f -d
 /usr/bin/git pull origin master
