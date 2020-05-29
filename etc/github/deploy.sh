360f470df05cd06ae7cd6f148b4250cdc1deda78
Jiho Lee <>
2020-05-29 15:59:26 -0700

    Update deploy shell script

--- etc/github/deploy.sh
+++ etc/github/deploy.sh
@@ -1,3 +1,3 @@
-/usr/bin/git reset --hard origin/master
-# /usr/bin/git clean -f -d
-/usr/bin/git pull origin master
+sudo /usr/bin/git reset --hard origin/master
+# sudo /usr/bin/git clean -f -d
+sudo /usr/bin/git pull origin master
