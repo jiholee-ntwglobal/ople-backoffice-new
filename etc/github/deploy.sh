e2228a0e354a343cfce6eed88fb111df3b0dcbaa
Jiho Lee <>
2020-05-28 21:14:39 -0700

    Update script to deploy test

--- etc/github/deploy.sh
+++ etc/github/deploy.sh
@@ -1,5 +1,7 @@
-eval $(ssh-agent)
+# eval $(ssh-agent)
 # ssh-add -k ~/.ssh/id_rsa_ntwglobal_ople-backoffice
 # /usr/bin/git reset --hard origin/master
 # /usr/bin/git clean -f -d
-/usr/bin/git pull origin master
+# /usr/bin/git pull origin master
+
+pwd
