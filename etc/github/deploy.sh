cb5809f0c68210e5b14e93310e7b809573c60363
Jiho Lee <jiho.lee@ntwglobal.com>
2020-05-30 07:51:53 +0900

    Update to fix gitignore from production

--- etc/github/deploy.sh
+++ etc/github/deploy.sh
@@ -1,5 +1,3 @@
-/usr/bin/git config --global user.name "Jiho Lee"
-/usr/bin/git config --global user.email "jiho.lee@ntwglobal.com"
 /usr/bin/git reset --hard origin/master
 # /usr/bin/git clean -f -d
 /usr/bin/git pull origin master
